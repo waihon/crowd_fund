c6ab08b4809fad45cb4d1c47a89c8ba6945cff42
Waihon Yew <yewwaihon@gmail.com>
2015-05-20 00:14:48 +0800

    Finish 3 - Numbers and Strings

---
+++ crowdfund.rb
@@ -0,0 +1,7 @@
+project1 = "Project ABC"
+funding1 = "1000"
+project2 = "Project LMN"
+project3 = "Project XYZ"
+
+puts "#{project1} has $#{funding1} in funding."
+puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
